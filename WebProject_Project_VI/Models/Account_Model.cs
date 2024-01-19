632ba3bd0d3d07da911b340d4f68820428533e64
Minh Nguyen <minhlenguyen7@gmail.com>
2024-01-19 12:07:13 -0500

    Implement the Struct Models for the Projects

---
+++ WebProject_Project_VI/Models/Account_Model.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject_Project_VI.Models
+{
+    public class Account_Model
+    {
+        [Required]
+        public string? Username { get; set; } = String.Empty;           // Username of the account
+        [Required]
+        public string? Password { get; set; } = String.Empty;           // Password of the account
+        [Required]
+        public string? AuthorName { get; set; } = String.Empty;         // Name of the author
+        public List<Post_Model>? post_Models { get; set; }              // List of posts corresponding to the author
+    }
+}
---
+++ WebProject_Project_VI/Models/Post_Model.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProject_Project_VI.Models
+{
+    public class Post_Model
+    {
+        [Required]
+        public string? Title { get; set; } = String.Empty;          // Title of the post
+        [Required]
+        public string? Content { get; set; } = String.Empty;        // Content of the post
+        [Required]
+        public string? Author { get; set; } = String.Empty;         // Username of the author           **Note: Foreign key with Account_Model.AuthorName **
+        public int? Number_Of_Likes { get; set; } = 0;              // Number of likes of the post
+        public int? Number_Of_DisLikes { get; set; } = 0;           // Number of dislikes of the post
+        public bool? Is_Public { get; set; } = false;               // Visibility of the post
+        public int? Number_Of_Visits { get; set; } = 0;             // Number of visits of the post
+        [Required]
+        public DateTime? Date { get; set; }                         // Date of the post
+    }
+}
