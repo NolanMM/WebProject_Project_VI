1e20c665fdf04973c2ba95495b09b52b3769da46
Liam Knapp <Lknapp1110@conestogac.on.ca>
2024-01-23 14:36:21 -0500

    made the accordions dynamic and get populated by the data passed from controller. moved java scrip to seprate file.

--- WebProject_Project_VI/Controllers/HomeController.cs
+++ WebProject_Project_VI/Controllers/HomeController.cs
@@ -15,7 +15,17 @@ namespace WebProject_Project_VI.Controllers
 
         public IActionResult Index()
         {
-            return View();
+
+            // Simulated data - replace this with your actual data retrieval logic
+            List<Post> posts = new List<Post>
+            {
+            new Post { AuthorName = "Author1", PostTitle = "Title1", Content = "Content1", LikeCount = 10, DislikeCount = 0, ViewCount = 100 },
+            new Post { AuthorName = "Author2", PostTitle = "Title2", Content = "Content2", LikeCount = 0, DislikeCount = 0, ViewCount = 0 },
+            new Post { AuthorName = "Author3", PostTitle = "Title3", Content = "Content3", LikeCount = 0, DislikeCount = 0, ViewCount = 0 },
+            new Post { AuthorName = "Liam", PostTitle = "Trafalgar Law is best character", Content = "Trafalgar D. Water Law, more commonly known as just Trafalgar Law and by his epithet as the Surgeon of Death, is a pirate from the North Blue and the captain and doctor of the Heart Pirates. He is one of twelve pirates who are referred to as the Worst Generation. He became one of the Seven Warlords of the Sea during the timeskip,but his position was revoked for allying with the Straw Hat Pirates. Law, like many other pirates, dreams of finding the One Piece, while also desiring to know the purpose of the Will of D.", LikeCount = 99999, DislikeCount = 0, ViewCount = 999999999 }
+            };
+
+            return View(posts);
         }
 
         public IActionResult Login()
---
+++ WebProject_Project_VI/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace WebProject_Project_VI
+{
+    public class Post
+    {
+        public string AuthorName { get; set; }
+        public string PostTitle { get; set; }
+        public string Content { get; set; }
+        public int LikeCount { get; set; }
+        public int DislikeCount { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
