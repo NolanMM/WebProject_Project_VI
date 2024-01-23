96f8e9ddf4453d758070d73d9dbe7e6fa723ec5b
Liam Knapp <Lknapp1110@conestogac.on.ca>
2024-01-23 11:51:32 -0500

    Created the start of the Ui layout. Incorporated basic logic that still needs lots of work.

--- WebProject_Project_VI/Controllers/HomeController.cs
+++ WebProject_Project_VI/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebProject_Project_VI.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
